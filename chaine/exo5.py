4e88361b2b4001b752fcdac13914fef140a2c5e7
0q3i <nicolas521janini@gmail.com>
2024-11-12 09:31:31 +0100

    fini

--- chaine/exo5.py
+++ chaine/exo5.py
@@ -1,5 +1,7 @@
+from cellul import *
+
 def concatener_inverser(l1,l2):
      if l1 is None:
           return l2
      else:
-          return concatener_inverser(concatener_inverser(l1.suivante,l1),l2)
+          return concatener_inverser(l1.suivante,Cellule(l1.valeur,l2))
