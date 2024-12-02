df789c6d2aafad58671d32f32fa7e37c4359c495
0q3i <nicolas521janini@gmail.com>
2024-12-02 16:32:43 +0100

    good

--- arbre/exo7.py
+++ arbre/exo7.py
@@ -1,11 +1,12 @@
 from noeud import *
+from math import *
 
-def parfait(h):
+def parfait(h,n=1):
     if h==1:
-        return Noeud(None,None,None)
+        return Noeud(None,n,None)
     else:
-        return Noeud(parfait(h-1),None,parfait(h-1))
+        return Noeud(parfait(h-1,n+1),n,parfait(h-1,n+1))
     
 #jeu de test
 
-assert parfait(2) == Noeud(Noeud(None,None,None),None,Noeud(None,None,None))
+assert parfait(2) == Noeud(Noeud(None,2,None),1,Noeud(None,2,None))
